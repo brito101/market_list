0ed0729809cd3ba9394f698b83c4328afac7b42a
brito101 <rodrigobrito101@hotmail.com>
2023-01-13 21:29:05 -0300

    Add refresh indicator

--- lib/ui/list_screen.dart
+++ lib/ui/list_screen.dart
@@ -21,30 +21,33 @@ class _ListScreenState extends State<ListScreen> {
         centerTitle: true,
         backgroundColor: Colors.blueGrey,
       ),
-      body: ListView.separated(
-        separatorBuilder: (context, index) => Divider(color: Colors.blueGrey),
-        itemCount: items.length,
-        itemBuilder: (context, index) {
-          final item = items[index];
+      body: RefreshIndicator(
+        onRefresh: _refresh,
+        child: ListView.separated(
+          separatorBuilder: (context, index) => Divider(color: Colors.blueGrey),
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            final item = items[index];
 
-          return ListTile(
-              leading: CircleAvatar(
-                backgroundColor: Colors.blueGrey,
-                child: IconTheme(
-                  data: IconThemeData(color: Colors.white),
-                  child: Icon(item.isDone ? Icons.done_all : Icons.done),
+            return ListTile(
+                leading: CircleAvatar(
+                  backgroundColor: Colors.blueGrey,
+                  child: IconTheme(
+                    data: IconThemeData(color: Colors.white),
+                    child: Icon(item.isDone ? Icons.done_all : Icons.done),
+                  ),
                 ),
-              ),
-              title: Text(
-                item.title,
-                style: TextStyle(color: Colors.blueGrey),
-              ),
-              onTap: () {
-                setState(() {
-                  items[index].isDone = !items[index].isDone;
+                title: Text(
+                  item.title,
+                  style: TextStyle(color: Colors.blueGrey),
+                ),
+                onTap: () {
+                  setState(() {
+                    items[index].isDone = !items[index].isDone;
+                  });
                 });
-              });
-        },
+          },
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _addItem,
@@ -63,5 +66,22 @@ class _ListScreenState extends State<ListScreen> {
     setState(() {
       items.add(item!);
     });
+  }
+
+  Future<void> _refresh() async {
+    await Future.delayed(Duration(seconds: 1));
+
+    setState(() {
+      items.sort((a, b) {
+        if (a.isDone && !b.isDone) {
+          return 1;
+        } else if (!a.isDone && b.isDone) {
+          return -1;
+        } else {
+          return 0;
+        }
+      });
+    });
+    return Future.value();
   }
 }
